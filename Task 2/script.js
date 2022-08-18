244dedf5c1371491ac90e207798925e23d4876b7
sniuraz <sniuraz@gmail.com>
2022-08-18 10:24:32 +0300

    skaiciuoja JS, atvaizduoja HTML

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,16 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+
+// taikomes
+const btnEl = document.getElementById('btn__element');
+const stateEl = document.getElementById('btn__state');
+let count = document.getElementById('btn__state').childElementCount;
+
+btnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    count += 1; // skaiciujam
+
+    stateEl.innerHTML = count; // atvaizduojam
+});
