b12b824e2a46406834c75a6404f49cf918b9492d
sniuraz <sniuraz@gmail.com>
2022-08-19 14:20:04 +0300

    funkcija grazina vienu atveju true, kitu - false

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,28 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget) {
+        this.pavadinimas = title;
+        this.rezisierius = director;
+        this.kaina = budget;
+    }
+    wasExpensive() {
+        if (this.kaina > 100000000) {
+            console.log(`${this.pav} was expensive to build:`, true);
+        } else {
+            console.log(`${this.pav} was expensive to build:`, false);
+        }
+    }
+}
+
+const movie1 = new Movie('pirmas', 'Antanas', 100000001);
+const movie2 = new Movie('antras', 'Antanas', 90000034);
+
+// pilna konstruktoriaus info
+// console.log(movie1);
+// console.log(movie2);
+
+movie1.wasExpensive();
+movie2.wasExpensive();
