ce0992d5e3b991a422b52e57b5ebdd1bd00b9746
sniuraz <sniuraz@gmail.com>
2022-08-19 20:54:13 +0300

    ES5 veikiantys 4 metodai

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator() {
+    this.sum = function (a, b) {
+        return a + b;
+    };
+
+    this.subtraction = function (a, b) {
+        return a - b;
+    };
+
+    this.multiplication = function (a, b) {
+        return a * b;
+    };
+
+    this.division = function (a, b) {
+        return a / b;
+    };
+}
+
+const skaiciai = new Calculator();
+console.log('skaiciai.sum() ===', skaiciai.sum(4, 2));
+console.log('skaiciai.subtraction() ===', skaiciai.subtraction(4, 2));
+console.log('skaiciai.multiplication() ===', skaiciai.multiplication(4, 2));
+console.log('skaiciai.division() ===', skaiciai.division(4, 2));
