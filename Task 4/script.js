2f906fa3cb970a19fcee52614747401c9bd0644c
sniuraz <sniuraz@gmail.com>
2022-08-21 19:39:13 +0300

    JS funkcijos, css style

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,25 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const carsContainerEl = document.getElementById('output');
+
+function carData(url) {
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log('data ===', data);
+            data.forEach(makeOneCarHtml); // creates all
+        })
+        .catch((err) => console.warn(err));
+}
+
+function makeOneCarHtml(carObj) {
+    const divEl = document.createElement('div');
+    divEl.className = 'post';
+    divEl.innerHTML = `<h2>${carObj.brand}</h2>
+    <h3>${carObj.models.join(', ')}</h3>`;
+    carsContainerEl.appendChild(divEl);
+    return divEl;
+}
+
+carData(ENDPOINT);
