a8dc49ef6b7bc67d86be006cae1b578806c84a8d
sniuraz <sniuraz@gmail.com>
2022-08-21 11:49:43 +0300

    JS funkcijos, css style

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,3 +12,35 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const btnEl = document.getElementById('btn');
+const pEl = document.getElementById('message');
+
+const usersContainerEl = document.getElementById('output'); // hiden
+
+btnEl.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    usersData(ENDPOINT);
+
+    btnEl.style.display = 'none';
+    pEl.style.display = 'none';
+});
+
+function usersData(url) {
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log('data ===', data);
+            data.forEach(makeOneUserHtml); // creates all users list
+        })
+        .catch((err) => console.warn(err));
+}
+
+function makeOneUserHtml(userObj) {
+    const divEl = document.createElement('div');
+    divEl.className = 'post';
+    divEl.innerHTML = `<img src="${userObj.avatar_url}" alt="avatar" />
+     <h3>${userObj.login}</h3>`;
+    usersContainerEl.appendChild(divEl);
+    return divEl;
+}
